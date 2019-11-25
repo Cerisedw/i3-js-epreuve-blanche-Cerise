aa6c337cffc3f9fb6438138d5c0cfa2569f7bd9d
CeriseDW <carnetsdecerise@gmail.com>
2019-11-25 10:25:09 +0100

    ajout du bouton enlever maki du panier pour chaque maki

--- src/panierCrea.js
+++ src/panierCrea.js
@@ -1,18 +1,26 @@
 import { makis } from './app/data/makis';
+import { enleverMaki } from './viderPanier';
 
 
 const creaPanier = (obj) => {
   const divMaki = document.createElement('div');
   divMaki.className = 'divMaki';
+
+  const btnSuppMaki = document.createElement('button');
+  btnSuppMaki.innerText = 'Enlever du panier';
+  btnSuppMaki.addEventListener('click', enleverMaki);
+
   const nomMaki = document.createElement('p');
   const total = document.getElementById('prixTot');
   const result = parseFloat(total.innerText) + (obj.prix * 8);
   total.innerText = result.toFixed(2);
   nomMaki.innerText = obj.nom;
+
+
   const prix = document.createElement('p');
   prix.className = 'prixMaki';
   prix.innerText = obj.prix * 8;
-  divMaki.append(nomMaki, prix);
+  divMaki.append(nomMaki, prix, btnSuppMaki);
   return divMaki;
 };
 
@@ -22,5 +30,12 @@ export const getMaki = (idString) => {
   const idNmb = parseInt(idString, 10);
   const maki = makis.filter((m) => m.id === idNmb);
   console.log(maki);
-  panier.appendChild(creaPanier(maki[0]));
+  const makiClone = { ...maki };
+  panier.appendChild(creaPanier(makiClone[0]));
 };
+
+
+const btnSuppMaki = document.createElement('button');
+btnSuppMaki.addEventListener('click', (e) => {
+  console.log(e.target);
+});
--- src/viderPanier.js
+++ src/viderPanier.js
@@ -1,7 +1,15 @@
 export const viderPanier = (nomDeClasse) => {
   const makis = document.getElementsByClassName(nomDeClasse);
-  console.log(makis[0].parentNode);
   while (makis.length > 0) {
     makis[0].parentNode.removeChild(makis[0]);
   }
+};
+
+
+export const enleverMaki = (event) => {
+  const totalMin = document.getElementById('prixTot');
+  const prixMaki = parseFloat(event.target.parentNode.children[1].innerText);
+  totalMin.innerText = (parseFloat(totalMin.innerText) - prixMaki).toFixed(2);
+  const makiClick = event.target.parentNode;
+  event.target.parentNode.parentNode.removeChild(makiClick);
 };
